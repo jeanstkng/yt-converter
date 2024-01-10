c91e1f15e4df924d0b04413826dbc3a81e3e3270
JeanStkng <jeancast.jg@gmail.com>
2024-01-09 20:01:23 -0500

    Added support for video too

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,10 +1,12 @@
-import { h } from "preact";
-import { useState } from "preact/hooks";
+import { h, type Ref } from "preact";
+import { useRef, useState } from "preact/hooks";
 
 const Download = () => {
   const [urlInput, setUrlInput] = useState<string>("");
   const [isInvalidUrlId, setIsInvalidUrlId] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const dropdownRef: Ref<HTMLDivElement> = useRef(null);
 
   const handleUrlInputChange = (event: Event) => {
     setIsInvalidUrlId(false);
@@ -14,7 +16,7 @@ const Download = () => {
     }
   };
 
-  const downloadFromUrl = async () => {
+  const downloadFromUrl = async (isVideo: boolean) => {
     if (urlInput.length < 10) {
       setIsInvalidUrlId(true);
     }
@@ -23,8 +25,11 @@ const Download = () => {
     const match = urlInput.match(regex);
 
     if (match) {
+      toggleDropdown();
       setIsLoading(true);
-      const response: any = await fetch(`download/${match[1]}`);
+      const response: any = await fetch(
+        `download?url=${match[1]}&type=${isVideo ? "video" : "audio"}`
+      );
       if (response.isError) {
         setIsInvalidUrlId(true);
         console.error(response.error);
@@ -42,7 +47,7 @@ const Download = () => {
 
       const downloadLink: HTMLAnchorElement = document.createElement("a");
       downloadLink.href = urlAudio;
-      downloadLink.download = "YourVideo.mp3";
+      downloadLink.download = isVideo ? "Video.mp4" : "Audio.mp3";
       setIsLoading(false);
 
       downloadLink.click();
@@ -51,26 +56,67 @@ const Download = () => {
     }
   };
 
+  const toggleDropdown = () => {
+    if (dropdownRef.current) {
+      setIsDropdownOpen(!isDropdownOpen);
+      if (!isDropdownOpen) {
+        dropdownRef.current["classList"].remove("hidden");
+      } else {
+        dropdownRef.current["classList"].add("hidden");
+      }
+    }
+  };
+
   return (
     <section class="text-center text-black grid mt-4">
       <input
         id="yt-url"
-        class="rounded-md w-1/2 h-8 p-2 mx-auto text-gray-800 text-xl grid-cols-12 border-green-950 border-2"
+        class="rounded-md w-3/4 md:w-1/2 h-8 p-2 mx-auto text-gray-800 text-xl grid-cols-12 border-green-950 border-2"
         onChange={handleUrlInputChange}
       />
       <button
         id="download-btn"
         class={`${
           !isLoading && "hover:bg-green-500"
-        } align-middle rounded-md w-1/4 h-14 mx-auto mt-3 mb-4 text-black font-semibold text-xl max-sm:text-sm bg-green-300 grid-cols-6 border-2 border-black`}
-        onClick={downloadFromUrl}
+        } align-middle rounded-md w-2/3 md:w-1/4 h-14 mx-auto mt-3 mb-1 text-black font-semibold text-xl max-sm:text-sm bg-green-300 grid-cols-6 border-2 border-black`}
         disabled={isLoading}
+        onClick={toggleDropdown}
       >
         <span class={`inline-block align-middle mb-2 text-center`}>
           Convert
         </span>
         {isLoading && <div class="loader ml-4"></div>}
       </button>
+
+      <div
+        ref={dropdownRef}
+        id="dropdown-menu"
+        class="hidden align-middle mx-auto w-48 rounded-md shadow-lg bg-green-300 ring-1 ring-black ring-opacity-5"
+        disabled={isLoading}
+      >
+        <div
+          class="py-2 p-2"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="dropdown-button"
+        >
+          <a
+            class="flex rounded-md px-4 py-2 text-sm text-black hover:bg-green-500 active:bg-blue-100 cursor-pointer"
+            role="menuitem"
+            onClick={() => downloadFromUrl(false)}
+          >
+            MP3 - Audio
+          </a>
+          <a
+            class="flex rounded-md px-4 py-2 text-sm text-black hover:bg-green-500 active:bg-blue-100 cursor-pointer"
+            role="menuitem"
+            onClick={() => downloadFromUrl(true)}
+          >
+            MP4 - Video
+          </a>
+        </div>
+      </div>
+
       {isInvalidUrlId && (
         <div class="rounded-md bg-red-400 w-1/5 max-sm:w-1/2 max-md:h-12 mx-auto max-sm:h-24 my-4">
           <div class="text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] max-md:text-lg max-xs:text-sm my-2 px-1">
---
+++ src/pages/download.ts
@@ -0,0 +1,51 @@
+export const prerender = false;
+import type { APIRoute } from "astro";
+import streamToBlob from "stream-to-blob";
+import ytdl, { type Filter } from "ytdl-core";
+
+interface ContentType {
+  contentType: string;
+  filter: Filter;
+}
+
+const contentType: Record<string, ContentType> = {
+  audio: {
+    contentType: "audio/mpeg",
+    filter: "audioonly",
+  },
+  video: {
+    contentType: "video/mp4",
+    filter: "videoandaudio",
+  },
+};
+
+export const GET: APIRoute = async ({ url }) => {
+  const reqUrl: string = url.searchParams.get("url") || "";
+  const type: string = url.searchParams.get("type") || "audio";
+
+  try {
+    const responseYt: any = ytdl(reqUrl, {
+      filter: contentType[type].filter,
+    });
+
+    const blob: Blob = await streamToBlob(responseYt);
+
+    return new Response(blob, {
+      headers: {
+        "Content-Type": contentType[type].contentType,
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        isError: true,
+        error,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+};
